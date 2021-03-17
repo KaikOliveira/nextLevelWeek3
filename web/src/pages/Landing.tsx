4791f501150d7e7de8959fc71f2ac1ab74e39850
KaikOliveira <kaik.oliveiraa76@gmail.com>
2021-03-17 20:32:33 -0300

    up

--- web/src/pages/Landing.tsx
+++ web/src/pages/Landing.tsx
@@ -8,26 +8,31 @@ import '../styles/pages/landing.css';
 
 import logoImg from '../imagens/logo.svg';
 
-export default function Landing () {
-    return (
-        <div id="page-landing">
-        <div className="content-wrapper">
-          <img src={logoImg} alt="Happy" />
-  
-          <main>
-            <h1>Leve felicidade para o mundo</h1>
-            <p>Visite ofanatos e mude o dia de muitas crianças.</p>
-          </main>
-  
-          <div className="location">
-            <strong>São Paulo</strong>
-            <span>Quatá</span>
-          </div>
-  
-          <Link to="/app" className="enter-app">
-            <FiArrowRight size={26} color="rgba(0,0,0, 0.6)" />
-          </Link>
+export default function Landing() {
+  return (
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy" />
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          {/*<p>Visite ofanatos e mude o dia de muitas crianças.</p>*/}
+          <p>Joao o mais noia de todos</p>
+          <a href="https://www.instagram.com/mariajulia.placido/">Insta da mais gata</a>
+
+          <p>Lucas BALA BALA via dm</p>
+          <a href="https://www.instagram.com/pedro.boim/">Link para DM</a>
+        </main>
+
+        <div className="location">
+          <strong>São Paulo</strong>
+          <span>Quatá</span>
         </div>
+
+        <Link to="/app" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0,0,0, 0.6)" />
+        </Link>
       </div>
-    );
+    </div>
+  );
 }
